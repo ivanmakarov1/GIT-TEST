a8f4e3a9846668a565fd59c9c6eaa4efd4d9e774
ivanmakarov1 <ivanmakarov1@yandex.ru>
2022-01-12 19:32:23 +0300

    et4y

--- unit_09.js
+++ unit_09.js
@@ -50,7 +50,13 @@ document.querySelector('.b-4').onclick = f4;
 function f5() {
     let elem = document.querySelector('.out-4');
     let contains = elem.classList.contains('bg-orange');
-    console.log(contains);
+    //document.getElement = contains.innerHTML('.out-5');
+    if(contains === true){
+        document.querySelector(".out-5").innerHTML = true;
+    }
+    else{
+        document.querySelector(".out-5").innerHTML = false;
+    }
 }
 
 document.querySelector('.b-5').onclick = f5;
