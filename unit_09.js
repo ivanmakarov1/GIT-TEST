b02b75119b55f50080ef12a926a887b1a3c85f70
ivanmakarov1 <ivanmakarov1@yandex.ru>
2022-01-14 18:21:56 +0300

    567

--- unit_09.js
+++ unit_09.js
@@ -60,7 +60,8 @@ document.querySelector('.b-5').onclick = f5;
 // По нажатию .b-6 запускайте функцию f6, которая выводит в .out-6 количество параграфов с классом .p-6.
 
 function f6() {
-let elem = document.querySelector('.out-6');
+    let a = document.querySelectorAll('.p-6').length;
+    document.querySelector('.out-6').innerHTML += a;
 
 }
 
